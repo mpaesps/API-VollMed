13360cbbec8c705216763645e6ce355e17719c09
Mayara Paes <mpaesps133@gmail.com>
2023-04-11 08:52:23 -0300

    controller paciente, configuraçao do banco de dados

--- src/main/java/med/voll/api/endereco/dadosEndereco.java
+++
@@ -1,4 +0,0 @@
-package med.voll.api.endereco;
-
-public record dadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
-}
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,6 +1,6 @@
 package med.voll.api.medico;
 
-import med.voll.api.endereco.dadosEndereco;
+import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, dadosEndereco endereco) {
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
 }
